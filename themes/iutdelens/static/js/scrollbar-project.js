af90ea57476ded46f47f4c480f4c43bfe5edaf6b
AymericJak <aymericjakpro@gmail.com>
2023-06-28 16:31:44 +0200

    Change JS scroller

--- themes/iutdelens/static/js/scrollbar-project.js
+++ themes/iutdelens/static/js/scrollbar-project.js
@@ -1,15 +1,13 @@
-window.addEventListener("DOMContentLoaded", (e) => {
-    const scroller = document.querySelector(".scroller");
+const scroller = document.querySelector(".scroller");
 
-    function updateScroller() {
-        let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-        let scrollTop = document.documentElement.scrollTop;
-        scroller.style.width = `${(scrollTop / height) * 100}%`;
-    }
+function updateScroller() {
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollTop = document.documentElement.scrollTop;
+    scroller.style.width = `${(scrollTop / height) * 100}%`;
+}
 
-    window.addEventListener("scroll", updateScroller);
-    window.addEventListener("resize", updateScroller); // Gérer les changements de taille de fenêtre
+window.addEventListener("scroll", updateScroller);
+window.addEventListener("resize", updateScroller); // Gérer les changements de taille de fenêtre
 
-    // Appeler la fonction une fois au chargement initial de la page
-    updateScroller();
-});
+// Appeler la fonction une fois au chargement initial de la page
+updateScroller();
